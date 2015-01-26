dd7c6385e09bfc458ebb9502dcadb9f4f4f1b4c0
ignaciodiazmaurino <ignaciodiazmaurino@gmail.com>
2015-01-26 18:44:05 -0300

    Calculos físcos

--- js/calculos.js
+++ js/calculos.js
@@ -1,5 +1,35 @@
     var GRAVEDAD = 9.8;
 
+    var valoresCalculo = {
+
+        gravedad: GRAVEDAD,
+        /*
+         Datos gas
+         */
+        densidadGas: 0,
+        alturaGas: 0,
+
+        /*
+         Datos Liquido
+         */
+        densidadLiquido: 0,
+        alturaInicial: 0,
+        presion: 0,
+
+        /*
+        Datos Tanque
+        */
+        areaOrificio: 0,
+        alturaTubo: 0,
+        areaBaseTanque: 0,
+
+        /*
+        Salida
+         */
+        velocidadSalida: 0
+
+    };
+
     /**
      * Velocidad final
      * @param {number} vx velocidad inicial horizontal
@@ -32,4 +62,45 @@
             x: x + vx * t + .5 * ax * Math.pow(t, 2),
             y: y + vy * t + .5 * ay * Math.pow(t, 2)
         }
+    }
+
+    /*
+     * Calculamos la velocidad de salida del orificio
+     *
+     * Calculamos el caudal
+     *
+     * Ya sabiendo sacamos: Volumen de liquido que se reduce para la proxima ejecucion y calculamos las nuevas alturas para la proxima ejecucion
+     *
+     * */
+    function calculosTanque(valoresCalculo) {
+    	//var altura = valoresEntrada.alturaInicial;
+    	//var velocidad = valoresEntrada.velocidad;
+
+        //valoresCalculo.velocidad =
+
+    }
+
+    function velocidadSalidaTanqueConTapa(valoresCalculo){
+
+        var calculoPresionGas = math.compile("densidadGas * gravedad * alturaGas");
+        valoresCalculo.presionGas = calculoPresionGas.eval(valoresCalculo).toNumber();
+
+        var calculoPresionSalida = math.compile("presionGas + densidadLiquido * gravedad * alturaInicial - 0.5 * densidadLiquido * square( velocidadSalida ) - densidadLiquido * gravedad * alturaTubo");
+        valoresCalculo.presionSalida = calcularPresionSalida.eval(valoresCalculo).toNumber();
+
+        var calcularVelocidad = math.compile("sqrt( (( 2 * ( presionGas - presionSalida ) ) / densidadLiquido) + 2 * gravedad * alturaInicial)");
+        valoresCalculo.velocidad = calcularVelocidad.eval(valoresCalculo).toNumber();
+
+        var calculoCaudal = math.compile("areaOrificio * velocidad");
+        var caudal = calculoCaudal.eval(valoresCalculo).toNumber();
+
+        var volumenARestar = caudal / 60;
+
+    }
+
+    function velocidadSalidaTanqueSinTapa(valoresCalculo){
+
+        var calcularVelocidad = math.compile("sqrt( 2 *  gravedad * alturaInicial)");
+        valoresCalculo.velocidad = calcularVelocidad.eval(valoresCalculo).toNumber();
+
     }
