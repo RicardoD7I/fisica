c4761546f65e9a3e534f1ba1277e6ba2d508bbb3
Maximiliano Stranich <mstranich@gmail.com>
2015-04-13 21:41:13 -0300

    Arreglada directiva de Rango de Números para que los botones de incrementer y decrementar sean más cómodos de usar.

--- js/simulador.directives.js
+++ js/simulador.directives.js
@@ -3,35 +3,50 @@
  */
 "use strict";
 
-angular.module('simulador').directive('numberRange', [function () {
-    return {
-        restrict: 'A',
-        replace: true,
-        scope: {model: '=ngModel', min: '=?', max: '=?', step:'=?', unit: '@?'},
-        templateUrl: 'view/numberRange.html',
-        link: function (scope, element, attrs) {
-            scope.elemId = attrs.id;
-            scope.elemName = attrs.name;
-            element.removeAttr('id').removeAttr('name');
+angular.module('simulador').directive('numberRange', [
+    '$interval',
+    function ($interval) {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {model: '=ngModel', min: '=?', max: '=?', step: '=?', unit: '@?'},
+            templateUrl: 'view/numberRange.html',
+            link: function (scope, element, attrs) {
+                scope.elemId = attrs.id;
+                scope.elemName = attrs.name;
+                element.removeAttr('id').removeAttr('name');
 
-            attrs.$observe('ngRequired', function (newValue) {
-                scope.isRequired = newValue;
-            });
+                attrs.$observe('ngRequired', function (newValue) {
+                    scope.isRequired = newValue;
+                });
 
-            scope.inc = function () {
-                if (!angular.isDefined(scope.max) || angular.isDefined(scope.max) && scope.model < scope.max) {
-                    scope.model = math.eval(scope.model + '+' + (scope.step || 1)).toNumber();
-                }
-            };
+                scope.inc = function () {
+                    if (!angular.isDefined(scope.max) || angular.isDefined(scope.max) && scope.model < scope.max) {
+                        scope.model = math.eval(scope.model + '+' + (scope.step || 1)).toNumber();
+                    }
+                };
 
-            scope.dec = function () {
-                if (!angular.isDefined(scope.min) || angular.isDefined(scope.min) && scope.model > scope.min) {
-                    scope.model =  math.eval(scope.model + '-' + (scope.step || 1)).toNumber();
-                }
-            };
+                scope.dec = function () {
+                    if (!angular.isDefined(scope.min) || angular.isDefined(scope.min) && scope.model > scope.min) {
+                        scope.model = math.eval(scope.model + '-' + (scope.step || 1)).toNumber();
+                    }
+                };
+
+                var autoRepeater = null;
+                scope.mouseDown = function (action) {
+                    if (!autoRepeater) autoRepeater = $interval(action, 100);
+                };
+
+                scope.mouseUp = function () {
+                    if (!!autoRepeater) {
+                        $interval.cancel(autoRepeater);
+                        autoRepeater = null;
+                    }
+                };
+            }
         }
     }
-}]);
+]);
 
 
 angular.module('simulador').directive('context2d', [function () {
